3d66a0124dfc155f969c7cdc0becffd50ab2b8b3
Uppal <26uppalmaxwell@gmail.com>
2024-02-19 14:20:30 -0500

    Added Gas Level Alert - Stable

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -11,11 +11,18 @@ def gasLevelGauge():
     return currentGasLevel
 
 #Function that lists Gas Stations, randomly choosing one and returning its value
-
 def listOfGasStations():
     gasStationList = ["Shell","Speedway","Marathon","Circle K","Moble","CostCo","Meijer","7-Eleven"]
     gasStationsNearby = random.choice(gasStationList)
     return gasStationsNearby
 
-print(gasLevelGauge())
-print(listOfGasStations())
+#Function will call the gasLevelGauge to determine our gas level and then find a close gas station
+#by calling the listOfGasStations function if we are on Low or Quarter Tank or Empty
+def gasLevelAlert():
+    milesToGasStationLow = random.uniform(0,25)
+    milesToGasStationQuarterTank = random.uniform(25.1,50)
+    #gasLevelGauge = gasLevelGauge()
+    print(milesToGasStationLow)
+    print(milesToGasStationQuarterTank)
+
+gasLevelAlert()
