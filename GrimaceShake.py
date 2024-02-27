128fd3f1d594d66e31e32ee44a3187d5416708e7
Uppal <26uppalmaxwell@gmail.com>
2024-02-27 13:36:00 -0500

    Updated VRS Function with ellifs and an else - Stable

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -18,16 +18,32 @@ weatherAlert = weather()
 
 def vehicleResponseSystem():
     if weatherAlert == "snowy":
-        print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of",weatherAlert,
+        print("\nNational Weather Service has updated our alarm by 30 minutes because of the forecast of",weatherAlert,
               "weather conditions.\n")
         print("VRS has been engaged only allowing you to drive 50mph.")
     elif weatherAlert == "blizzardy":
-        print("\nNational Weather Service has updated our alarm by 45 minutes because of the forcast of",weatherAlert,
+        print("\nNational Weather Service has updated our alarm by 45 minutes because of the forecast of",weatherAlert,
               "weather conditions.\n")
         print("VRS has been engaged only allowing you to drive 40mph")
     elif weatherAlert == "rainy":
-        print("\nNational Weather Service has updated our alarm by 10 minutes because of the forcast of",weatherAlert,
+        print("\nNational Weather Service has updated our alarm by 10 minutes because of the forecast of",weatherAlert,
               "weather conditions.\n")
         print("VRS has been engaged only allowing you to drive 60mph")
+    elif weatherAlert == "foggy":
+        print("\nNational Weather Service has updated our alarm by 20 minutes because of the forecast of",weatherAlert,
+              "weather conditions.\n")
+        print("VRS has been engaged only allowing you to drive 55mph")
+    elif weatherAlert == "windy":
+        print("\nNational Weather Service has updated our alarm by 5 minutes because of the forecast of",weatherAlert,
+              "weather conditions.\n")
+        print("VRS has been engaged only allowing you to drive 70mph")
+    elif weatherAlert == "icy":
+        print("\nNational Weather Service has updated our alarm by 60 minutes because of the forecast of",weatherAlert,
+              "weather conditions.\n")
+        print("VRS has been engaged only allowing you to drive 30mph")
+    else:
+        print("\nNational Weather Service forecasts",weatherAlert,
+              "weather conditions.\n")
+        print("VRS has been disengaged! Drive at your own risk.")
 
 vehicleResponseSystem()
